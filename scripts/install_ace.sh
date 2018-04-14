4951333dfedb4ababeb1e126bd4e46f04b27d8e1
seogeekk <seogeekk@hotmail.com>
2018-04-15 00:22:51 +1000

    Remove aceapp references

--- scripts/install_ace.sh
+++ scripts/install_ace.sh
@@ -2,8 +2,6 @@
 
 echo "Installing ABO"
 
-type apache2
-[[ $? -ne 0 ]] && { echo "Apache2 seems to be not installed"; exit 1; }
 type nginx
 [[ $? -ne 0 ]] && { echo "Nginx seems to be not installed"; exit 1; }
 ls -ltr /var/www/html >/dev/null
@@ -28,18 +26,12 @@ if [[ -z $ACE_EXTERNAL_IP_ADDRESS ]]; then
 fi
 
 ACEAPIDIR=/usr/pkg/aceapi
-ACEAPPDIR=/usr/pkg/aceapp
 
 echo ">> Checkout repositories"
 if [[ ! -d /usr/pkg/aceapi ]]; then
 	mkdir -p /usr/pkg
 	cd /usr/pkg
 	git clone https://github.com/seogeekk/aceapi.git
-fi
-if [[ ! -d /usr/pkg/aceapp ]]; then
-	mkdir -p /usr/pkg
-	cd /usr/pkg
-	git clone https://github.com/seogeekk/aceapp.git
 fi
 
 echo ">> Updating aceapi"
@@ -57,37 +49,11 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-echo ">> Updating aceapp"
-cd $ACEAPPDIR
-git pull
-if [[ $? -ne 0 ]]; then
-	git stash
-	git pull
-	[[ $? -ne 0 ]] && { echo "Error: Failed Updating aceapp"; exit 1; }
-fi
-echo ">> Run npm install"
-npm install
-if [[ $? -ne 0 ]]; then
-	echo "Error: Failed node packages update"
-	exit 1
-fi
-echo ">> Run bower install"
-bower install --allow-root
-if [[ $? -ne 0 ]]; then
-	echo "Error: Failed bower packages update"
-	exit 1
-fi
-
-echo ">> Copy aceapp to /var/www/html"
-cp -rf $ACEAPPDIR/app/* /var/www/html
-[[ $? -ne 0 ]] && { echo "Error: Failed copying aceapp files to /var/www/html"; exit 1; }
-
 echo ">> Run replace of external address"
 replacehost.pl $ACE_EXTERNAL_IP_ADDRESS
 [[ $? -ne 0 ]] && { echo "Error: Failed replacing $ACE_EXTERNAL_IP_ADDRESS"; exit 1; }
 
 echo ">> Check for localhost references"
-grep localhost  /var/www/html/components/services/*.js
 grep localhost /usr/pkg/aceapi/routes/handlers/*.js
 
 
@@ -103,10 +69,6 @@ fi
 echo ">> Restart Nginx"
 service nginx restart
 [[ $? -ne 0 ]] && { echo "Error: Nginx restart failed"; exit 1; }
-
-echo ">> Restart apache2"
-service apache2 restart
-[[ $? -ne 0 ]] && { echo "Error: Apache2 restart failed"; exit 1; }
 
 echo
 echo "*****************" 
