49b82534455e317ceb28643fcb75a01500601eff
seogeekk <seogeekk@hotmail.com>
2018-04-15 23:10:51 +1000

    Correct port on install script

--- scripts/replacehost.pl
+++ scripts/replacehost.pl
@@ -15,4 +15,4 @@ my $replace = quotemeta($external_ip);
 
 `perl -i -ple "s/http\:/https\:/g"  /usr/pkg/aceapi/routes/handlers/*.js`;
 
-`perl -i -ple "s/localhost\:80/$replace/g" /usr/pkg/aceapi/routes/handlers/*.js`;
+`perl -i -ple "s/localhost\:8000/$replace/g" /usr/pkg/aceapi/routes/handlers/*.js`;
